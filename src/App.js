5e9e87bd595f8ae4fd18e65dcb52a0f6eedcab51
d930379 <you@example.com>
2020-08-21 16:35:10 +0530

    useeffect

--- src/App.js
+++ src/App.js
@@ -5,14 +5,26 @@ function App() {
   
   const [name,setName] = useState('');
   const renderCount = useRef(0);
+  const inputRef = useRef();
+  const prevName = useRef();
+
   useEffect(()=>{
     renderCount.current= renderCount.current+1;
   })
+
+  useEffect(()=>{
+    prevName.current = name
+  },[name]) // when u want to run this change in here when name changes that's why in bracket [name]
+
+  function focus(){
+    inputRef.current.focus();
+  }
   return (
   <>
-    <input type="text"  onChange={e=>setName(e.target.value)}/>
-    <div>my name is {name}</div>
+    <input ref={inputRef} type="text"  onChange={e=>setName(e.target.value)}/>
+  <div>my name is {name} and it used to be {prevName.current}</div>
     <div>render count {renderCount.current}</div>
+    <button onClick={focus}>focus</button>
   </>
   )
 }
