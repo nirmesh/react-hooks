73a0d43b782e5d9c417318e1883023177b23be40
d930379 <you@example.com>
2020-08-23 14:32:35 +0530

    created custom hooks

--- src/App.js
+++ src/App.js
@@ -1,66 +1,134 @@
-import React,{useEffect,useRef,useState} from 'react';
+import React, { useEffect, useRef, useState, useReducer } from 'react';
 import './App.css';
-import sample from './sample.mp3'
+import sample from './sample.mp3';
+import userInput from './hooks/UserInput';
+
+
+// function reducer(state,action) {
+//   switch (action.type) {
+//     case 'increment':
+//       return { count: state.count + 1 }
+//     case 'decrement':
+//       return { count: state.count - 1 }
+//     default:
+//       return state;
+//   }
+// }
+
 
 function App() {
 
-  const audioRef = useRef(null);
-  useEffect(()=>{
-    audioRef.current.play();
-  },[ ])
 
-  const handleClick = ()=>{
-    audioRef.current.pause();
+  //const [firstName,setFirstName] = useState()
+  //const [lastName,setLastName] = useState()
+  const [firstName,bindFirstName,resetFirstName] = userInput();
+  const [lastName,bindLastName,resetLastName] = userInput();
+
+  const  submitHandler=(e)=>{
+    e.preventDefault();
+    alert("hello "+firstName+" "+lastName)
+    resetFirstName()
+    resetLastName()
   }
-
   return (
-    <>
-    
-    <audio src={sample} ref={audioRef} />
-    <button onClick={handleClick}>Stop</button>
-    </>
+    <div>
+      <form onSubmit={submitHandler}>
+        <div>
+          {/* FirstName <input type="text" value={firstName}  onChange={e=>setFirstName(e.target.value)}/> */}
+      First Name <input type="text" {...bindFirstName} />  
+        </div>
+        <div>
+        Last Name<input type="text" {...bindLastName}/>
+        </div>
+        <button>submit</button>
+      </form>
+
+    </div>
+
   )
+  // const [state, dispatch] = useReducer(reducer, { count: 0 });
+  // function incrementCount() {
+  //   dispatch({ type: 'increment' })
+  // }
 
-// const [resourceType,setResourceType]=useState('posts');
-// const [windowWidth,setWindowWidth]=useState(window.innerWidth);
+  // function decrementCount() {
+  //   dispatch({ type: 'decrement' })
+  // }
+//=======
+  // const [count,setCount] = useState(5);
+  // const incrementCount = ()=>{
+  //   //previouscount = previouscount+1;
+  //   setCount(xy=>xy+1);
+  // }
+  // const decrementCount = ()=>{
+  //   //previouscount = previouscount-1;
+  //   setCount(previouscount=>previouscount-1);
+  // }
 
-// const [items,setItems]=useState([]);
+  // return (
+  //   <>
+  //   <button onClick={incrementCount}>+</button> {state.count}<button onClick={decrementCount}>-</button>
+  //   </>
+  // )
+  //=====
 
-// const handleResize = ()=>{
-//   setWindowWidth(window.innerWidth);
-// }
+  // const audioRef = useRef(null);
+  // useEffect(()=>{
+  //   audioRef.current.play();
+  // },[ ])
 
-// useEffect(()=>{
-//   window.addEventListener('resize',handleResize)
-//   return ()=>{
-//     window.removeEventListener('resize',handleResize); // use it for cleaner place
-//   }
-// },[])
+  // const handleClick = ()=>{
+  //   audioRef.current.pause();
+  // }
 
-// useEffect(()=>{
-//   console.log("resource type changed");
-//   fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
-//   .then(response => response.json())
-//   .then(json => setItems(json))
-// },[resourceType])// if we pass only empty array then it will execute only on mount
+  // return (
+  //   <>
+
+  //   <audio src={sample} ref={audioRef} />
+  //   <button onClick={handleClick}>Stop</button>
+  //   </>
+  // )
+  //===========
+  // const [resourceType,setResourceType]=useState('posts');
+  // const [windowWidth,setWindowWidth]=useState(window.innerWidth);
+
+  // const [items,setItems]=useState([]);
+
+  // const handleResize = ()=>{
+  //   setWindowWidth(window.innerWidth);
+  // }
+
+  // useEffect(()=>{
+  //   window.addEventListener('resize',handleResize)
+  //   return ()=>{
+  //     window.removeEventListener('resize',handleResize); // use it for cleaner place
+  //   }
+  // },[])
+
+  // useEffect(()=>{
+  //   console.log("resource type changed");
+  //   fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+  //   .then(response => response.json())
+  //   .then(json => setItems(json))
+  // },[resourceType])// if we pass only empty array then it will execute only on mount
 
 
-// return (
-//   <>
+  // return (
+  //   <>
 
-// <div>{windowWidth}</div>
-//   <div>
-//     <button onClick={()=>setResourceType('posts')}>Posts</button>
-//     <button onClick={()=>setResourceType('users')}>Users</button>
-//     <button onClick={()=>setResourceType('comments')}>Comments</button>
+  // <div>{windowWidth}</div>
+  //   <div>
+  //     <button onClick={()=>setResourceType('posts')}>Posts</button>
+  //     <button onClick={()=>setResourceType('users')}>Users</button>
+  //     <button onClick={()=>setResourceType('comments')}>Comments</button>
 
-//   </div>
-//   <h1>{resourceType}</h1>
-//   {items.map(item=>{
-//     return (<div>{JSON.stringify(item)}</div>)
-//   })}
-//   </>
-// )
+  //   </div>
+  //   <h1>{resourceType}</h1>
+  //   {items.map(item=>{
+  //     return (<div>{JSON.stringify(item)}</div>)
+  //   })}
+  //   </>
+  // )
   /*
   const [name,setName] = useState('');
   const renderCount = useRef(0);
